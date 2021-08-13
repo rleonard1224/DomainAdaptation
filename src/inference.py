1dba8cceabd95f65939ac39e4c733722f6bf47f9
rleonard1224 <72096022+rleonard1224@users.noreply.github.com>
2021-08-13 13:09:00 -0400

    Python CycleGAN files

---
+++ src/inference.py
@@ -0,0 +1,28 @@
+'''
+Script to inference a dataset through a trained generator network
+'''
+
+# Import python libraries
+import os, shutil
+
+# Specify directories
+imagesdir = '../dataset/images/'
+cyclegandir = '../../pytorch-CycleGAN-and-pix2pix/'
+datasetsdir = cyclegandir + 'datasets/test/'
+checkpointsdir = cyclegandir + 'checkpoints/test/'
+modeldir = '../model/'
+srcdir = ''
+
+# Transfer input dataset to test set
+print(os.getcwd())
+os.makedirs(datasetsdir + 'testA/')
+for file in sorted(os.listdir(imagesdir)):
+    shutil.copy(imagesdir + file, datasetsdir + 'testA/')
+
+# Transfer model to checkpoints directory
+os.makedirs(checkpointsdir)
+for file in sorted(os.listdir(modeldir)):
+    shutil.copy(modeldir + file, checkpointsdir + 'latest_net_G.pth')
+
+# Transfer the updated networks.py file
+shutil.copy(srcdir + 'networks.py', cyclegandir + 'models/')
