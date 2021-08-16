243782b1577c715e709332b105eb11a1022b54e6
rleonard1224 <72096022+rleonard1224@users.noreply.github.com>
2021-08-15 22:33:21 -0400

    Add files via upload

--- src/inference.py
+++ src/inference.py
@@ -31,8 +31,15 @@ shutil.copy(srcdir + 'networks.py', cyclegandir + 'models/')
 os.chdir('pytorch-CycleGAN-and-pix2pix')
 os.system('python test.py --dataroot datasets/test/testA --name test --model test --no_dropout --netG resnet_3blocks')
 
-# Copy original dataset to new folder
+# Copy original dataset to new folder and create new directory for gan-inferenced images
 shutil.copytree('../data/dataset/', '../data/gandataset/')
+os.mkdir('../data/gandataset/ganimages/')
 
 # Copy gan-inferenced dataset to new folder
-shutil.copytree('results/test/test_latest/images', '../data/gandataset/ganimages/')
+for ifile, filename in enumerate(sorted(os.listdir('results/test/test_latest/images'))):
+    if 'fake' in filename:
+        filebase = filename.split('_')[0]
+        shutil.copy('results/test/test_latest/images'+filename,'../data/gandataset/ganimages/'+filebase+'.png')
+
+
+# shutil.copytree('results/test/test_latest/images', '../data/gandataset/ganimages/')
